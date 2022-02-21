18807b775c84e60ac1d1a2b258ff51f1789f06f9
Joel Sequeira <joelseq96@gmail.com>
2022-02-21 15:24:22 -0800

    Add nvim-tree plugin

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -25,8 +25,6 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 15<cr>", opts)
-
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
 keymap("n", "<C-Down>", ":resize -2<CR>", opts)
@@ -71,3 +69,6 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 --- Telescope
 keymap("n", "<leader>f", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>", opts)
 keymap("n", "<leader>F", "<cmd>Telescope live_grep theme=ivy<cr>", opts)
+
+--- Nvimtree
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
