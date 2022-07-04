b5ec1cde1370fea5c2ca895f2647d0c7141eceae
Joel Sequeira <joelseq96@gmail.com>
2022-07-04 00:05:58 -0700

    (neovim/lsp) lsp installer setup improvements

--- config/nvim/lua/user/lsp/lsp-installer.lua
+++ config/nvim/lua/user/lsp/lsp-installer.lua
@@ -3,25 +3,41 @@ if not status_ok then
   return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-  local opts = {
+local servers = {
+  "sumneko_lua",
+  "tsserver",
+  "jsonls",
+  "yamlls",
+  "rust_analyzer",
+  "taplo",
+}
+
+lsp_installer.setup()
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = require("user.lsp.handlers").capabilities,
   }
 
-  if server.name == "jsonls" then
+  if server == "jsonls" then
     local jsonls_opts = require("user.lsp.settings.jsonls")
     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
   end
 
-  if server.name == "sumneko_lua" then
+  if server == "sumneko_lua" then
     local sumneko_opts = require("user.lsp.settings.sumneko_lua")
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
 
-  if server.name == "rust_analyzer" then
+  if server == "rust_analyzer" then
     require("rust-tools").setup({
       tools = {
         on_initialized = function()
@@ -49,8 +65,6 @@ lsp_installer.on_server_ready(function(server)
     goto continue
   end
 
-  -- This setup() function is exactly the same as lspconfig's setup function.
-  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  server:setup(opts)
+  lspconfig[server].setup(opts)
   ::continue::
-end)
+end
