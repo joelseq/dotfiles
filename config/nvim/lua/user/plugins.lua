9d2ce75cd95da2196ce8867a7bdca4dd6cd3c1ba
Joel Sequeira <joelseq96@gmail.com>
2022-08-18 14:52:39 -0400

    (neovim/telescope) Add fzf-native

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -112,6 +112,10 @@ return packer.startup(function(use)
 
 	--- Telescope
 	use("nvim-telescope/telescope.nvim")
+	use({
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "make",
+	})
 
 	--- Treesitter
 	use({
