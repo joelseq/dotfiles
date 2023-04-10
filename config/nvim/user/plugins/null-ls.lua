eae74d60a2fe723fe3b4ce8931e1dc84618a808f
Joel Sequeira <joelseq96@gmail.com>
2023-04-10 01:28:29 -0400

    (neovim) Update config to astronvim v3 (#1)

---
+++ config/nvim/user/plugins/null-ls.lua
@@ -0,0 +1,44 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, config)
+    -- config variable is the default configuration table for the setup function call
+    local null_ls = require "null-ls"
+    local formatting = null_ls.builtins.formatting
+
+    -- Check supported formatters and linters
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    config.sources = {
+      -- Set a formatter
+      formatting.stylua,
+      -- null_ls.builtins.formatting.prettier,
+      formatting.prettierd.with {
+        env = {
+          PRETTIERD_LOCAL_PRETTIER_ONLY = 1,
+        },
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "css",
+          "scss",
+          "less",
+          "html",
+          "json",
+          "jsonc",
+          "yaml",
+          "markdown",
+          "markdown.mdx",
+          "graphql",
+          "handlebars",
+          "ruby",
+        },
+      },
+      formatting.stylua,
+      null_ls.builtins.code_actions.gitsigns,
+    }
+    return config -- return final config table
+  end,
+}
---
+++ config/nvim/user/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "ruby",
+      "rust",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+}
