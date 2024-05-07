8cd596712d715cf83fb518e5f4217571c3fe35f7
Joel Sequeira <joelseq96@gmail.com>
2024-05-06 23:44:39 -0400

    (neovim) Disable lsp-signature

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -8,11 +8,11 @@ return {
 
   -- "andweeb/presence.nvim",
   "rebelot/kanagawa.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function() require("lsp_signature").setup() end,
+  -- },
 
   {
     "kylechui/nvim-surround",
