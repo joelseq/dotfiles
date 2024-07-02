86e0b7b5dc6cfaf022b1a46871c0386fcd40ecbd
Joel Sequeira <joelseq96@gmail.com>
2024-07-02 12:45:11 -0400

    change theme to tokyonight

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -7,6 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.pack.go" },
   -- import/override with your plugins folder
