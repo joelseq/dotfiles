4c7ca5a731e7c110142dc8b955f38b4d44f9adf6
Joel Sequeira <joelseq96@gmail.com>
2024-07-13 18:20:46 -0400

    Work in progress migration

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
+
+vim.keymap.set("i", "<C-J>", "<C-N>", { remap = true })
+
+vim.keymap.set("i", "<C-K>", "<C-P>", { remap = true })
