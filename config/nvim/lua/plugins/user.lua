084c306a4fc0e87e29ff81ff473125cc80c702a3
Joel Sequeira <joelseq96@gmail.com>
2024-07-20 01:25:38 -0400

    Change keymaps for cmp and telescope to astronvim

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -1,3 +1,11 @@
+local function has_words_before()
+  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+local function is_visible(cmp)
+  return cmp.core.view:visible() or vim.fn.pumvisible() == 1
+end
+
 return {
   {
     "folke/tokyonight.nvim",
@@ -34,5 +42,117 @@ return {
   {
     "f-person/git-blame.nvim",
     event = "VeryLazy",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+      local cmp = require("cmp")
+      opts.preselect = cmp.PreselectMode.None
+      opts.mapping = {
+        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-P>"] = cmp.mapping(function()
+          if is_visible(cmp) then
+            cmp.select_prev_item()
+          else
+            cmp.complete()
+          end
+        end),
+        ["<C-N>"] = cmp.mapping(function()
+          if is_visible(cmp) then
+            cmp.select_next_item()
+          else
+            cmp.complete()
+          end
+        end),
+        ["<C-K>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        ["<C-J>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<C-U>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+        ["<C-D>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-Y>"] = cmp.config.disable,
+        ["<C-E>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
+        ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = false }), { "i", "c" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if is_visible(cmp) then
+            cmp.select_next_item()
+          elseif vim.api.nvim_get_mode().mode ~= "c" and vim.snippet and vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if is_visible(cmp) then
+            cmp.select_prev_item()
+          elseif vim.api.nvim_get_mode().mode ~= "c" and vim.snippet and vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+    },
+    -- change some options
+    opts = function()
+      local actions = require("telescope.actions")
+      local open_selected = function(prompt_bufnr)
+        local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+        local selected = picker:get_multi_selection()
+        if vim.tbl_isempty(selected) then
+          actions.select_default(prompt_bufnr)
+        else
+          actions.close(prompt_bufnr)
+          for _, file in pairs(selected) do
+            if file.path then
+              vim.cmd("edit" .. (file.lnum and " +" .. file.lnum or "") .. " " .. file.path)
+            end
+          end
+        end
+      end
+      local open_all = function(prompt_bufnr)
+        actions.select_all(prompt_bufnr)
+        open_selected(prompt_bufnr)
+      end
+      return {
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-J>"] = actions.move_selection_next,
+              ["<C-K>"] = actions.move_selection_previous,
+              ["<CR>"] = open_selected,
+              ["<M-CR>"] = open_all,
+            },
+            n = {
+              q = actions.close,
+              ["<CR>"] = open_selected,
+              ["<M-CR>"] = open_all,
+            },
+          },
+        },
+      }
+    end,
   },
 }
