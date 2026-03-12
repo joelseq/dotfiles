11f9985e40cdc920bed7a45e36975f5b66f783c2
Joel Sequeira <joelseq96@gmail.com>
2026-03-12 18:17:57 -0400

    Make setup script idempotent

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone https://github.com/joelseq/dotfiles.git ~/dotfiles
+if [[ -d ~/dotfiles ]]; then
+  git -C ~/dotfiles pull
+else
+  git clone https://github.com/joelseq/dotfiles.git ~/dotfiles
+fi
 ~/dotfiles/bootstrap.sh
